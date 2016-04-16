00675987e4be6aa2cdeee584fe8b549aed74ce9a
DTallay <youdianre123@ailyun.com>
2016-04-16 22:02:00 +0800

    登陆界面

--- app/src/main/java/com/dtalliance/activity/MainActivity.java
+++ app/src/main/java/com/dtalliance/activity/MainActivity.java
@@ -1,23 +1,48 @@
 package com.dtalliance.activity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
+import android.view.WindowManager;
 
 import com.dtalliance.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        goLogin();
+        getWindow().setFormat(PixelFormat.RGBA_8888);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DITHER);
+
+        //Display the current version number
+        PackageManager pm = getPackageManager();
+        try {
+            PackageInfo pi = pm.getPackageInfo("org.wordpress.android", 0);
+//            TextView versionNumber = (TextView) findViewById(R.id.versionNumber);
+//            versionNumber.setText("Version " + pi.versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                /* Create an Intent that will start the Main WordPress Activity. */
+                goLogin();
+            }
+        }, 2900); //2900 for release
+
     }
 
-    private void goLogin(){
+    public void goLogin(){
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
+        finish();
     }
 }
